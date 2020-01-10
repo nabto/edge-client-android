df9527c1ed28a44d63d0b741f57689d0829706da
Michael <mkm@nabto.com>
2020-01-10 09:21:26 +0100

    added build version

--- wrapper/src/main/java/com/nabto/edge/client/ConnectionEventsCallback.java
+++
@@ -1,9 +0,0 @@
-package com.nabto.edge.client;
-
-public abstract class ConnectionEventsCallback {
-    public static int CONNECTED = com.nabto.edge.client.swig.ConnectionEventsCallback.CONNECTED();
-
-    public static int CLOSED = com.nabto.edge.client.swig.ConnectionEventsCallback.CLOSED();
-    public static int CHANNEL_CHANGED = com.nabto.edge.client.swig.ConnectionEventsCallback.CHANNEL_CHANGED();
-    public abstract void onEvent(int event);
-}
