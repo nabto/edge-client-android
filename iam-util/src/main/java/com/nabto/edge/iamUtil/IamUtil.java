e0dbf7fe75b8281c7f410ae8d91cc68e065555ee
unknown <ahmadsaleh@quark.dev>
2022-07-26 17:46:29 +0200

    update docs for IamUtil.java

--- iam-util/src/main/java/com/nabto/edge/iamUtil/IamUtil.java
+++ iam-util/src/main/java/com/nabto/edge/iamUtil/IamUtil.java
@@ -27,6 +27,8 @@ public abstract class IamUtil {
      *
      * Local open pairing uses the trusted local network (LAN) pairing mechanism. No password is required for pairing and no
      * invitation is needed, anybody on the LAN can initiate pairing.
+     * 
+     * This function blocks until it has succeeded.
      *
      * Read more here: https://docs.nabto.com/developer/guides/concepts/iam/pairing.html#open-local
      *
@@ -42,13 +44,27 @@ public abstract class IamUtil {
      */
     public abstract void pairLocalOpen(Connection connection, String desiredUsername);
 
-    public abstract void pairLocalOpenCallback(Connection connection, String desiredUsername, IamCallback cb);
+    /**
+     * Perform Local Open pairing, requesting the specified username. Runs asynchronously with a callback.
+     *
+     * Local open pairing uses the trusted local network (LAN) pairing mechanism. No password is required for pairing and no
+     * invitation is needed, anybody on the LAN can initiate pairing.
+     *
+     * Read more here: https://docs.nabto.com/developer/guides/concepts/iam/pairing.html#open-local
+     *
+     * @param connection An established connection to the device this client should be paired with
+     * @param desiredUsername Assign this username on the device if available (pairing fails with .USERNAME_EXISTS if not)
+     * @param callback The callback that will be run once the function has completed successfully or erroneously.
+     */
+    public abstract void pairLocalOpenCallback(Connection connection, String desiredUsername, IamCallback callback);
 
     /**
      * Perform Local Initial pairing, assigning the default initial username configured on the device (typically "admin").
      *
      * In this mode, the initial user can be paired on the local network without providing a username or password - and
      * only the initial user. This is a typical bootstrap scenario to pair the admin user (device owner).
+     * 
+     * This function blocks until it has succeeded.
      *
      * Read more here: https://docs.nabto.com/developer/guides/concepts/iam/pairing.html#initial-local
      *
@@ -62,6 +78,19 @@ public abstract class IamUtil {
      */
     public abstract void pairLocalInitial(Connection connection);
 
+    /**
+     * Perform Local Initial pairing, assigning the default initial username configured on the device (typically "admin"). Runs asynchronously with a callback.
+     *
+     * In this mode, the initial user can be paired on the local network without providing a username or password - and
+     * only the initial user. This is a typical bootstrap scenario to pair the admin user (device owner).
+     * 
+     * Error information is sent to the callback.
+     *
+     * Read more here: https://docs.nabto.com/developer/guides/concepts/iam/pairing.html#initial-local
+     *
+     * @param connection An established connection to the device this client should be paired with
+     * @param callback The callback that will be run once the function has completed successfully or erroneously.
+     */
     public abstract void pairLocalInitialCallback(Connection connection, IamCallback cb);
 
     /**
@@ -69,6 +98,8 @@ public abstract class IamUtil {
      *
      * In this mode a device has set a password which can be used in the pairing process to grant a client access to the
      * device. The client can pair remotely to the device if necessary; it is not necessary to be on the same LAN.
+     * 
+     * This function blocks until it has succeeded.
      *
      * Read more here: https://docs.nabto.com/developer/guides/concepts/iam/pairing.html#open-password
      *
@@ -87,6 +118,22 @@ public abstract class IamUtil {
      */
     public abstract void pairPasswordOpen(Connection connection, String desiredUsername, String password);
 
+
+    /**
+     * Perform Password Open pairing, requesting the specified username and authenticating using the specified password. Runs asynchronously with a callback.
+     *
+     * In this mode a device has set a password which can be used in the pairing process to grant a client access to the
+     * device. The client can pair remotely to the device if necessary; it is not necessary to be on the same LAN.
+     * 
+     * Error information is sent to the callback.
+     *
+     * Read more here: https://docs.nabto.com/developer/guides/concepts/iam/pairing.html#open-password
+     *
+     * @param connection An established connection to the device this client should be paired with
+     * @param desiredUsername Assign this username on the device if available (pairing fails with .USERNAME_EXISTS if not)
+     * @param password the common (not user-specific) password to allow pairing using Password Open pairing
+     * @param callback The callback that will be run once the function has completed successfully or erroneously.
+     */
     public abstract void pairPasswordOpenCallback(Connection connection, String desiredUsername, String password, IamCallback cb);
 
     /**
@@ -94,6 +141,8 @@ public abstract class IamUtil {
      *
      * In the Password invite pairing mode a user is required in the system to be able to pair: An existing user (or
      * the system autonomously) creates a username and password that is somehow passed to the new user (an invitation).
+     * 
+     * This function blocks until it has succeeded.
      *
      * Read more here: https://docs.nabto.com/developer/guides/concepts/iam/pairing.html#invite
      *
@@ -109,10 +158,27 @@ public abstract class IamUtil {
      */
     public abstract void pairPasswordInvite(Connection connection, String username, String password);
 
+    /**
+     * Perform Password Invite pairing, authenticating with the specified username and password. Runs asynchronously with a callback.
+     *
+     * In the Password invite pairing mode a user is required in the system to be able to pair: An existing user (or
+     * the system autonomously) creates a username and password that is somehow passed to the new user (an invitation).
+     * 
+     * Error information is sent to the callback.
+     *
+     * Read more here: https://docs.nabto.com/developer/guides/concepts/iam/pairing.html#invite
+     *
+     * @param connection An established connection to the device this client should be paired with
+     * @param username Username for the invited user
+     * @param password Password for the invited user
+     * @param callback The callback that will be run once the function has completed successfully or erroneously.
+     */
     public abstract void pairPasswordInviteCallback(Connection connection, String username, String password, IamCallback cb);
 
     /**
      * Retrieve a list of the available pairing modes on the device.
+     * 
+     * This function blocks until it has succeeded.
      *
      * @param connection An established connection to the device
      *
@@ -124,6 +190,8 @@ public abstract class IamUtil {
 
     /**
      * Retrieve device information that typically does not need a paired user.
+     * 
+     * This function blocks until it has succeeded.
      *
      * @param connection An established connection to the device
      *
@@ -133,10 +201,20 @@ public abstract class IamUtil {
      */
     public abstract DeviceDetails getDeviceDetails(Connection connection);
 
+    /**
+     * Retrieve device information that typically does not need a paired user. Runs asynchronously with a callback.
+     *
+     * Error information is sent to the callback.
+     * 
+     * @param connection An established connection to the device
+     * @param callback The callback that will be run once the function has completed successfully or erroneously.
+     */
     public abstract void getDeviceDetailsCallback(Connection connection, IamCallback<DeviceDetails> cb);
 
     /**
      * Query if the current user is paired or not on a specific device.
+     * 
+     * This function blocks until it has succeeded.
      *
      * @param connection An established connection to the device
      * @throws IAM_NOT_SUPPORTED if Nabto Edge IAM is not supported by the device
@@ -144,10 +222,20 @@ public abstract class IamUtil {
      */
     public abstract boolean isCurrentUserPaired(Connection connection);
 
+    /**
+     * Query if the current user is paired or not on a specific device. Runs asynchronously with a callback.
+     *
+     * Result and error information is sent to the callback.
+     * 
+     * @param connection An established connection to the device.
+     * @param callback The callback that will be run once the function has completed successfully or erroneously.
+     */
     public abstract void isCurrentUserPairedCallback(Connection connection, IamCallback<Boolean> cb);
 
     /**
      * Get details about a specific user.
+     * 
+     * This function blocks until it has succeeded.
      *
      * @param connection An established connection to the device
      *
@@ -159,10 +247,20 @@ public abstract class IamUtil {
      */
     public abstract IamUser getUser(Connection connection, String username);
 
+    /**
+     * Get details about a specific user. Runs asynchronously with a callback.
+     *
+     * Result and error information is sent to the callback.
+     * 
+     * @param connection An established connection to the device
+     * @param callback The callback that will be run once the function has completed successfully or erroneously.
+     */
     public abstract void getUserCallback(Connection connection, String username, IamCallback<IamUser> cb);
 
     /**
      * Get details about the user that has opened the current connection to the device.
+     * 
+     * This function blocks until it has succeeded.
      *
      * @param connection An established connection to the device
      *
@@ -172,10 +270,20 @@ public abstract class IamUtil {
      */
     public abstract IamUser getCurrentUser(Connection connection);
 
+    /**
+     * Get details about the user that has opened the current connection to the device. Runs asynchronously with a callback.
+     * 
+     * Result and error information is sent to the callback.
+     * 
+     * @param connection An established connection to the device
+     * @param callback The callback that will be run once the function has completed successfully or erroneously.
+     */
     public abstract void getCurrentUserCallback(Connection connection, IamCallback<IamUser> cb);
 
     /**
      * Create an IAM user on device.
+     * 
+     * This function blocks until it has succeeded.
      *
      * See https://docs.nabto.com/developer/guides/concepts/iam/intro.html for an intro to the concept of users and roles.
      *
@@ -191,10 +299,25 @@ public abstract class IamUtil {
      */
     public abstract void createUser(Connection connection, String username, String password, String role);
 
+    /**
+     * Create an IAM user on device. Runs asynchronously with a callback.
+     * 
+     * Result and error information is sent to the callback.
+     * 
+     * See https://docs.nabto.com/developer/guides/concepts/iam/intro.html for an intro to the concept of users and roles.
+     *
+     * @param connection An established connection to the device
+     * @param username Username for the new user
+     * @param password Password for the new user
+     * @param role IAM role for the new user
+     * @param callback The callback that will be run once the function has completed successfully or erroneously.
+     */
     public abstract void createUserCallback(Connection connection, String username, String password, String role, IamCallback cb);
 
     /**
      * Update an IAM user's password on device.
+     * 
+     * This function blocks until it has succeeded.
      *
      * @param connection An established connection to the device
      * @param username Username for the user that should have password updated
@@ -205,6 +328,16 @@ public abstract class IamUtil {
      */
     public abstract void updateUserPassword(Connection connection, String username, String password);
 
+    /**
+     * Update an IAM user's password on device. Runs asynchronously with a callback.
+     *
+     * Result and error information is sent to the callback.
+     * 
+     * @param connection An established connection to the device
+     * @param username Username for the user that should have password updated
+     * @param password New password for the user
+     * @param callback The callback that will be run once the function has completed successfully or erroneously.
+     */
     public abstract void updateUserPasswordCallback(Connection connection, String username, String password, IamCallback cb);
 
     /**
@@ -213,6 +346,8 @@ public abstract class IamUtil {
      * Known issue: This function currently assumes the user exists. To be able to interpret the
      * ROLE_DOES_NOT_EXIST code correctly, this assumption most hold. Later it can gracefully handle
      * non-existing users
+     * 
+     * This function blocks until it has succeeded.
      *
      * See https://docs.nabto.com/developer/guides/concepts/iam/intro.html for an intro to the concept of roles.
      *
@@ -227,10 +362,28 @@ public abstract class IamUtil {
      */
     public abstract void updateUserRole(Connection connection, String username, String role);
 
+    /**
+     * Update an IAM user's role on device. Runs asynchronously with a callback.
+     *
+     * Known issue: This function currently assumes the user exists. To be able to interpret the
+     * ROLE_DOES_NOT_EXIST code correctly, this assumption most hold. Later it can gracefully handle
+     * non-existing users
+     *
+     * Result and error information is sent to the callback.
+     * 
+     * See https://docs.nabto.com/developer/guides/concepts/iam/intro.html for an intro to the concept of roles.
+     *
+     * @param connection An established connection to the device
+     * @param username Username for the user that should have password updated
+     * @param role New role for the user
+     * @param callback The callback that will be run once the function has completed successfully or erroneously.
+     */
     public abstract void updateUserRoleCallback(Connection connection, String username, String role, IamCallback cb);
 
     /**
      * Update an IAM user's display name on device.
+     * 
+     * This function blocks until it has succeeded.
      *
      * @param connection An established connection to the device
      * @param username Username for the user that should have display name updated
@@ -242,10 +395,22 @@ public abstract class IamUtil {
      */
     public abstract void updateUserDisplayName(Connection connection, String username, String displayName);
 
+    /**
+     * Update an IAM user's display name on device. Runs asynchronously with a callback.
+     * 
+     * Result and error information is sent to the callback.
+     *
+     * @param connection An established connection to the device
+     * @param username Username for the user that should have display name updated
+     * @param displayName New display name
+     * @param callback The callback that will be run once the function has completed successfully or erroneously.
+     */
     public abstract void updateUserDisplayNameCallback(Connection connection, String username, String displayName, IamCallback cb);
 
     /**
      * Update an IAM user's username on device.
+     * 
+     * This function blocks until it has succeeded.
      *
      * @param connection An established connection to the device
      * @param username Username for the user that should have username updated
@@ -258,10 +423,22 @@ public abstract class IamUtil {
      */
     public abstract void renameUser(Connection connection, String username, String newUsername);
 
+    /**
+     * Update an IAM user's username on device. Runs asynchronously with a callback.
+     * 
+     * Result and error information is sent to the callback.
+     *
+     * @param connection An established connection to the device
+     * @param username Username for the user that should have username updated
+     * @param newUsername New username for the user
+     * @param callback The callback that will be run once the function has completed successfully or erroneously.
+     */
     public abstract void renameUserCallback(Connection connection, String username, String newUsername, IamCallback cb);
 
     /**
      * Delete the specified user from device.
+     * 
+     * This function blocks until it has succeeded.
      *
      * @param connection An established connection to the device
      *
@@ -272,5 +449,13 @@ public abstract class IamUtil {
      */
     public abstract void deleteUser(Connection connection, String username);
 
+    /**
+     * Delete the specified user from device. Runs asynchronously with a callback.
+     * 
+     * Result and error information is sent to the callback.
+     *
+     * @param connection An established connection to the device
+     * @param callback The callback that will be run once the function has completed successfully or erroneously.
+     */
     public abstract void deleteUserCallback(Connection connection, String username, IamCallback cb);
 }
