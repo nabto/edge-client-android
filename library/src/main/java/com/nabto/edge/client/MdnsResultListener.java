6e9c17165942c216b224f5097f115bb1d79e1369
Ulrik Gammelby <ug@nabto.com>
2022-09-29 13:09:59 +0200

    add missing nabtodoc

--- library/src/main/java/com/nabto/edge/client/MdnsResultListener.java
+++ library/src/main/java/com/nabto/edge/client/MdnsResultListener.java
@@ -1,5 +1,12 @@
 package com.nabto.edge.client;
 
+/**
+ * This interface specifies a callback function to receive mDNS results
+ */
 public interface MdnsResultListener {
+    /*
+     * The implementation is invoked when an mDNS result is ready
+     * @param result The callback result.
+     */
     public void onChange(MdnsResult result);
 }
