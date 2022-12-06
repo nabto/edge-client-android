7fe987c20fb2fd972023399df2cc94f4738c6bda
Ahmad Saleh <ahmadsaleh@quark.dev>
2022-12-06 17:56:49 +0100

    Add awaitGetAvailableRoles to iam-util-ktx

--- iam-util-ktx/src/main/java/com/nabto/edge/iamUtil/ktx/extensions.kt
+++ iam-util-ktx/src/main/java/com/nabto/edge/iamUtil/ktx/extensions.kt
@@ -155,6 +155,26 @@ suspend fun IamUtil.awaitGetDeviceDetails(
     }).get()
 }
 
+
+/**
+ * Retrieve the roles that a user can be assigned on this device.
+ * 
+ * This function is meant to be used in a Kotlin coroutine to suspend execution until the operation has completed.
+ *
+ * @param connection An established connection to the device
+ *
+ * @throws BLOCKED_BY_DEVICE_CONFIGURATION if the device configuration does not allow retrieving this list
+ * (requires the `IAM:ListRoles` permission)
+ * @throws IAM_NOT_SUPPORTED if Nabto Edge IAM is not supported by the device
+ */
+suspend fun IamUtil.awaitGetAvailableRoles(
+    connection: Connection
+): Array<String> {
+    return iamWrapper<Array<String>>({ callback ->
+        this@awaitGetAvailableRoles.getAvailableRolesCallback(connection, callback)
+    }).get()
+}
+
 /**
  * Query if the current user is paired or not on a specific device.
  *
