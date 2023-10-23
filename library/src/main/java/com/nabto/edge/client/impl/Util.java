7d046a147e89f487f16dabfffa09b9371601a197
Ulrik Gammelby <ug@nabto.com>
2023-10-23 21:39:54 +0200

    guard application callback with try-catch to log (and swallow) exception to prevent crash in native thread

--- library/src/main/java/com/nabto/edge/client/impl/Util.java
+++ library/src/main/java/com/nabto/edge/client/impl/Util.java
@@ -1,5 +1,9 @@
 package com.nabto.edge.client.impl;
 
+import android.util.Log;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.Optional;
 import com.nabto.edge.client.NabtoCallback;
 import com.nabto.edge.client.swig.FutureCallback;
@@ -10,7 +14,14 @@ public class Util {
         FutureCallback cb = new FutureCallback() {
             public void run(com.nabto.edge.client.swig.Status status) {
                 Optional<Void> opt = Optional.empty();
-                callback.run(status.getErrorCode(), opt);
+                try {
+                    callback.run(status.getErrorCode(), opt);
+                } catch (Throwable t) {
+                    StringWriter sw = new StringWriter();
+                    PrintWriter pw = new PrintWriter(sw);
+                    t.printStackTrace(pw);
+                    Log.e("nabto", "Callback into application from SDK threw an exception (will not be propagated to Nabto Client SDK as this will cause crash):  " + sw);
+                }
             }
         };
         return cb;
