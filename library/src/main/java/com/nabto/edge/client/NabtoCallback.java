96cfc8a6454878140d34981c7ebcd9837a8cf5e1
Michael <mkm@nabto.com>
2023-11-17 15:00:14 +0100

    document that callback exceptions will be thrown away.

--- library/src/main/java/com/nabto/edge/client/NabtoCallback.java
+++ library/src/main/java/com/nabto/edge/client/NabtoCallback.java
@@ -10,6 +10,8 @@ public interface NabtoCallback<T> {
     /**
      * Function called when the async operation is completed.
      *
+     * Exceptions thrown will be ignored by the caller as the caller has no meaningful way to handle the exception.
+     *
      * @param errorCode The resulting error code of the operation.
      * @param arg Argument containing the resulting data if any.
      */
