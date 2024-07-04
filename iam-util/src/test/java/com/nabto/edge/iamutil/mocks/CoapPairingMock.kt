f562e58db13c89d43b6782f1d07eada1a8d9f706
Michael <mkm@nabto.com>
2024-07-04 17:05:55 +0200

    test password invite pairing and get pairing modes

--- iam-util/src/test/java/com/nabto/edge/iamutil/mocks/CoapPairingMock.kt
+++ iam-util/src/test/java/com/nabto/edge/iamutil/mocks/CoapPairingMock.kt
@@ -1,6 +1,7 @@
 package com.nabto.edge.iamutil.mocks
 
 import com.nabto.edge.client.Coap
+import com.nabto.edge.client.Connection
 import io.mockk.every
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.cbor.Cbor
@@ -8,13 +9,14 @@ import kotlinx.serialization.encodeToByteArray
 
 @Serializable
     class PairingResponse(
-        val Modes: Array<String> = arrayOf("LocalInitial"),
+        val Modes: Array<String> = arrayOf(),
         val FriendlyName: String = "friendlyname",
         val DeviceId: String = "de-test",
         val ProductId: String = "pr-test",
         val AppVersion: String = "0.0.1",
         val AppName: String = "test",
-        val NabtoVersion: String = "0.0.0"
+        val NabtoVersion: String = "0.0.0",
+        val Unknown: String = ""
     )
 
 
@@ -29,3 +31,23 @@ import kotlinx.serialization.encodeToByteArray
         every { coap.responsePayload } returns ( responseData )
         return coap
     }
+
+fun mockCoapGetPairingError(connection: Connection, statusCode: Int) {
+    val coap : Coap = createCoapMock();
+    every { coap.execute() } returns ( Unit )
+    every { coap.responseStatusCode } returns ( statusCode )
+
+    every { connection.createCoap("GET", "/iam/pairing") } returns coap
+}
+
+fun mockCoapGetParing(connection : Connection, pairingResponse : PairingResponse = PairingResponse())
+{
+    val coap : Coap = createCoapMock();
+    every { coap.execute() } returns ( Unit )
+    every { coap.responseStatusCode } returns ( 205 )
+    //every { coap.responseContentFormat } returns (Coap.ContentFormat.APPLICATION_CBOR )
+    val responseData =  Cbor.encodeToByteArray(pairingResponse);
+    every { coap.responsePayload } returns ( responseData )
+
+    every { connection.createCoap("GET", "/iam/pairing") } returns coap
+}
