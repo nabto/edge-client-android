73c8ab32cffee26beb1aa311172d52a06ad1c30e
Ahmad Saleh <ahmadsaleh@quark.dev>
2025-08-25 10:04:35 +0200

    bump buildtools back down to 34

--- buildSrc/src/main/kotlin/com/nabto/edge/NabtoConfig.kt
+++ buildSrc/src/main/kotlin/com/nabto/edge/NabtoConfig.kt
@@ -2,7 +2,7 @@ package com.nabto.edge
 
 object NabtoConfig {
     const val compileSdk = 34
-    const val buildToolsVersion = "35.0.0"
+    const val buildToolsVersion = "34.0.0"
     const val minSdk = 24
     const val artifactGroup = "com.nabto.edge.client"
     const val testTargetSdk = compileSdk
