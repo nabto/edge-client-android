2370c76ae9c7da7458c6fd2cb590eb4aea2f5660
Ahmad Saleh <ahmadsaleh@quark.dev>
2025-08-25 15:56:00 +0200

    sdk and build tools to v36

--- buildSrc/src/main/kotlin/com/nabto/edge/NabtoConfig.kt
+++ buildSrc/src/main/kotlin/com/nabto/edge/NabtoConfig.kt
@@ -1,8 +1,8 @@
 package com.nabto.edge
 
 object NabtoConfig {
-    const val compileSdk = 34
-    const val buildToolsVersion = "34.0.0"
+    const val compileSdk = 36
+    const val buildToolsVersion = "36.0.0"
     const val minSdk = 24
     const val artifactGroup = "com.nabto.edge.client"
     const val testTargetSdk = compileSdk
